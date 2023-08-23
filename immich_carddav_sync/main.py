d0f6408b2f2278b71b7158a762b5f90aa6bd4e02
Daniele Ricci <daniele@casaricci.it>
2023-08-23 20:17:29 +0200

    Very wip, but it works (kind of)

--- immich_carddav_sync/main.py
+++ immich_carddav_sync/main.py
@@ -10,8 +10,8 @@ from vdirsyncer.storage import dav
 from vdirsyncer.vobject import Item
 
 from .immich_client import AuthenticatedClient
-from .immich_client.api.person import get_all_people
-from .immich_client.models import PeopleResponseDto
+from .immich_client.api.person import get_all_people, update_person
+from .immich_client.models import PeopleResponseDto, PersonResponseDto, PersonUpdateDto
 
 
 from .config import settings
@@ -66,13 +66,36 @@ async def fetch_immich_people(api_url: str, api_key: str):
         return names
 
 
+async def set_immich_birth_date(person_id: str, birth_date: str, api_url: str, api_key: str):
+    print('PUT /people/%s << {"birthDate":"%s"}' % (person_id, birth_date))
+    with AuthenticatedClient(base_url=api_url, auth_header_name="X-api-key", prefix="", token=api_key) as client:
+        dto = PersonUpdateDto(birth_date=datetime.date.fromisoformat(birth_date))
+        response: PersonResponseDto = await update_person.asyncio(id=person_id, json_body=dto, client=client)
+        # TODO response?
+
+
 async def async_main():
+    print("Fetching CardDAV address book...")
     addressbook = await fetch_carddav_addressbook(
         settings.carddav_url, settings.carddav_addressbook, settings.carddav_username, settings.carddav_password
     )
     print(addressbook)
+
+    print("Fetching people from Immich...")
     people = await fetch_immich_people(settings.immich_api_url, settings.immich_api_key)
     print(people)
+
+    for contact_name, contact_bday in addressbook.items():
+        if contact_name in people:
+            if contact_bday != people[contact_name][1]:
+                print("Setting birth date for %s to %s" % (contact_name, contact_bday))
+                await set_immich_birth_date(
+                    people[contact_name][0], contact_bday, settings.immich_api_url, settings.immich_api_key
+                )
+            else:
+                print("Birth date for %s is already %s - skipping" % (contact_name, contact_bday))
+        else:
+            print("Contact %s not found in Immich - skipping" % contact_name)
 
 
 def main():
