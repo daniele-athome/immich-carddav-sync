7a63d76349d866cccc81e2b4ee9557c961dd9ef5
Daniele Ricci <daniele@casaricci.it>
2026-07-03 15:13:44 +0200

    Code reformat

--- immich_carddav_sync/main.py
+++ immich_carddav_sync/main.py
@@ -7,8 +7,7 @@ import aiohttp
 import aiostream
 import uuid
 import vobject
-from immichpy.client.generated import PeopleResponseDto, PeopleUpdateDto, \
-    PeopleUpdateItem, BulkIdResponseDto
+from immichpy.client.generated import PeopleResponseDto, PeopleUpdateDto, PeopleUpdateItem, BulkIdResponseDto
 
 from vdirsyncer.cli import fetchparams
 from vdirsyncer.storage import dav
@@ -18,7 +17,6 @@ from immichpy import AsyncClient
 
 from .config import settings
 from .logger import init_logging
-
 
 logger = logging.getLogger("immich-carddav-sync")
 
@@ -58,7 +56,6 @@ async def fetch_carddav_addressbook(url: str, collection: str, username: str, pa
                     # no omit year, all fine!
                     pass
 
-
                 first_last = None
                 if hasattr(vcard, "n") and vcard.n.value:
                     # use the first and last name from the vcard
@@ -71,22 +68,24 @@ async def fetch_carddav_addressbook(url: str, collection: str, username: str, pa
                 if hasattr(vcard, "fn") and vcard.fn.value:
                     # use the formatted name from the vcard
                     fn = vcard.fn.value.strip()
-                
+
                 nickname = None
                 if hasattr(vcard, "nickname") and vcard.nickname.value:
                     # use the nickname from the vcard
                     nickname = vcard.nickname.value.strip()
 
-                contacts.append({
-                    "birthday": formatted_birthday,
-                    "fields": {
-                        # Order is important here: it also defines the priority for name matching.
-                        # Starting Python 3.7 dict has guaranteed insertion order.
-                        "fn": fn,
-                        "first_last": first_last,
-                        "nickname": nickname
+                contacts.append(
+                    {
+                        "birthday": formatted_birthday,
+                        "fields": {
+                            # Order is important here: it also defines the priority for name matching.
+                            # Starting Python 3.7 dict has guaranteed insertion order.
+                            "fn": fn,
+                            "first_last": first_last,
+                            "nickname": nickname,
+                        },
                     }
-                })
+                )
 
             except ValueError:
                 # invalid date
@@ -96,6 +95,7 @@ async def fetch_carddav_addressbook(url: str, collection: str, username: str, pa
                 pass
 
         return contacts
+
 
 async def fetch_immich_people(api_url: str, api_key: str):
     """
@@ -124,13 +124,15 @@ async def fetch_immich_people(api_url: str, api_key: str):
 async def set_immich_birth_date(person_id: uuid.UUID, birth_date: str, api_url: str, api_key: str):
     logger.debug('PUT /people/%s << {"birthDate":"%s"}', person_id, birth_date)
     async with AsyncClient(base_url=api_url, api_key=api_key) as client:
-        dto = PeopleUpdateDto(people=[PeopleUpdateItem(birthDate=datetime.date.fromisoformat(birth_date),
-                                                       id=person_id)])
+        dto = PeopleUpdateDto(
+            people=[PeopleUpdateItem(birthDate=datetime.date.fromisoformat(birth_date), id=person_id)]
+        )
         response: list[BulkIdResponseDto] = await client.people.update_people(dto)
         if not response or len(response) < 1 or not response[0].success:
             raise RuntimeError("Birth date was not set.")
 
-def match_person_to_first_contact(person_name: str, contacts: list) -> dict|None:
+
+def match_person_to_first_contact(person_name: str, contacts: list) -> dict | None:
     """
     Match a person name in Immich to the first contact that it can find in the address book.
     :return: the first matched contact, otherwise None.
@@ -141,10 +143,11 @@ def match_person_to_first_contact(person_name: str, contacts: list) -> dict|None
         logger.debug("Checking contact: %s", contact)
         for field, value in contact["fields"].items():
             if value == person_name:
-                logger.debug("Found matching person by %s: %s", field.replace('_', ' '), person_name)
+                logger.debug("Found matching person by %s: %s", field.replace("_", " "), person_name)
                 return contact
 
     return None
+
 
 async def async_main():
     logger.info("Fetching CardDAV address book...")
@@ -164,13 +167,15 @@ async def async_main():
             for person in people_data:
                 # people with same name will have same date of birth
                 if match["birthday"] != person[1]:
-                    logger.info("Found matching contact for %s (%s): %s", person_name, person[0], match['birthday'])
+                    logger.info("Found matching contact for %s (%s): %s", person_name, person[0], match["birthday"])
                     await set_immich_birth_date(
-                        person[0], match['birthday'], settings.immich_api_url, settings.immich_api_key
+                        person[0], match["birthday"], settings.immich_api_url, settings.immich_api_key
                     )
                 else:
-                    logger.info("Birth date for person %s (%s) is already %s - skipping" %
-                                (person_name, person[0], match['birthday']))
+                    logger.info(
+                        "Birth date for person %s (%s) is already %s - skipping"
+                        % (person_name, person[0], match["birthday"])
+                    )
 
         else:
             logger.info("Person %s not found in contacts - skipping" % person_name)
